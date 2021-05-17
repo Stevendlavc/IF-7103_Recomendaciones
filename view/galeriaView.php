72eb571302a5b984015ea66744f6c49bc81b47ec
MabelHeller <arguedas3023@gmail.com>
2021-05-17 01:19:34 -0600

    Cambios páginas Heller
    Cambios en el tamaño del fondo

--- view/galeriaView.php
+++ view/galeriaView.php
@@ -2,50 +2,50 @@
 require_once 'public/header.php';
 ?>
 
+
 <div class="jumbotron">
   <div class="container">
-    <h1>Galeria</h1> 
-    <p>Muestra imagenes y la descripción de los atractivos y actividades.</p>  
+    <h1>Mapa del sitio</h1> 
+    <p>Muestra como es la estructura del sitio con una descripcion de cada nodo y su link.</p>  
     </div>
 </div>
-
 <div class="containerRow">
+    <div class="containerRow">
     <h3 class="text-center">Atractivos</h3>
     <div class="row rowGaleria">
         <div class="col-sm-4 text-center">
-        	<h4 class="tituloMapa" href="index.php">Nombre del atractivo</h4>
+            <h4 class="tituloMapa" href="index.php">Nombre del atractivo</h4>
             <img src="public/css/img/manuelAntonio.jpg" style="width: 300px;">
-        	<p>Descripción del atractivo</p>
+            <p>Descripción del atractivo</p>
         </div>
-	  	<div class="col-sm-4 text-center">
-	  		<h4 class="tituloMapa" href="index.php">Nombre del atractivo</h4>
+        <div class="col-sm-4 text-center">
+            <h4 class="tituloMapa" href="index.php">Nombre del atractivo</h4>
             <img src="public/css/img/chirripo.jpg" style="width: 300px;">
             <p>Descripción del atractivo</p>
-	  	</div>
-	 	<div class="col-sm-4 text-center">
-	  		<h4 class="tituloMapa" href="index.php">Nombre del atractivo</h4>
+        </div>
+        <div class="col-sm-4 text-center">
+            <h4 class="tituloMapa" href="index.php">Nombre del atractivo</h4>
             <img src="public/css/img/tortuga.jpg" style="width: 300px;">
             <p>Descripción del atractivo</p>
-	  	</div>
+        </div>
     </div>
     <div class="row rowGaleria">
         <div class="col-sm-4 text-center">
-        	<h4 class="tituloMapa" href="index.php">Nombre del atractivo</h4>
+            <h4 class="tituloMapa" href="index.php">Nombre del atractivo</h4>
             <img src="public/css/img/volcanArenal.jpg" style="width: 300px;">
             <p>Descripción del atractivo</p>
         </div>
-	  	<div class="col-sm-4 text-center">
-	  		<h4 class="tituloMapa" href="index.php">Nombre del atractivo</h4>
+        <div class="col-sm-4 text-center">
+            <h4 class="tituloMapa" href="index.php">Nombre del atractivo</h4>
             <img src="public/css/img/volcanIrazu.jpg" style="width: 300px;">
             <p>Descripción del atractivo</p>
-	  	</div>
-	 	<div class="col-sm-4 text-center">
-	  		<h4 class="tituloMapa" href="index.php">Nombre del atractivo</h4>
+        </div>
+        <div class="col-sm-4 text-center">
+            <h4 class="tituloMapa" href="index.php">Nombre del atractivo</h4>
             <img src="public/css/img/manuelAntonio.jpg" style="width: 300px;">
             <p>Descripción del atractivo</p>
-	  	</div>
+        </div>
     </div>
-    
     <h3 class="text-center">Actividades</h3>
     <div class="row rowGaleria">
         <div class="col-sm-4 text-center">
@@ -81,6 +81,7 @@ require_once 'public/header.php';
             <p>Descripción de la actividad</p>
         </div>
     </div>
+</div>
 </div>
 
 <?php
