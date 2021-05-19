1aabfc9f3d6bbc0bf4defed839a449341a7c2c4f
MabelHeller <arguedas3023@gmail.com>
2021-05-19 01:09:31 -0600

    Pagina Galeria
    Efecto zoom en la pagina de la galeria

--- public/header.php
+++ public/header.php
@@ -8,6 +8,8 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="public/css/estilo.css"> 
+  <script type="text/javascript" src="public/js/js_sitio.js"></script> 
+
 </head>
 <body>
 
